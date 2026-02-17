c9896c808b0bdf806a2b5c57a4f913570950471a
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-02-17 11:21:50 +0300

    Added Styling to the App

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,18 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Counter App</h1>
-      <h2>{counter}</h2>
+    <div className='h-screen mt-10'>
+     <div className='p-2 border border-gray-200 rounded-lg shadow-lg w-1/2 min-w-75 mx-auto flex flex-col gap-3'>
+       <h1 className='text-center lg:text-4xl text-3xl font-semibold'>Counter App</h1>
+      <h2 className='text-3xl font-semibold text-amber-500 shadow-lg border border-gray-200 w-fit px-4 mx-auto'>{counter}</h2>
 
-      <div>
-        <button onClick={incrementHandler}>Increase +</button>
-        <button onClick={decrementHandler}>Decrease -</button>
-        <button onClick={addbyHandler}>Add by +10</button>
+      <div className='w-full flex gap-3 justify-center'> 
+        <button className='p-2 bg-green-500 text-white text-lg font-semibold rounded-sm' onClick={incrementHandler}>Increase +</button>
+        <button className='p-2 bg-green-500 text-white text-lg font-semibold rounded-sm' onClick={decrementHandler}>Decrease -</button>
+        <button className='p-2 bg-green-500 text-white text-lg font-semibold rounded-sm' onClick={addbyHandler}>Add by +10</button>
       </div>
 
+     </div>
     </div>
   )
 }
